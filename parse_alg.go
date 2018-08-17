315f1fb351d97932a30657d8e498d68af687d027
TobyLi <toby@emotech.co>
2018-08-17 15:40:58 +0100

    json file reading with GO

--- parse_alg.go
+++ parse_alg.go
@@ -1,6 +1,7 @@
 package main
 
 import "fmt"
+import "os"
 
 //define an Action structure
 type Action struct {
@@ -26,37 +27,47 @@ type Node struct {
 	children   map[int]int
 }
 
+//create method for Node class to be able to add children nodes
+func (n *Node) add_child(childID, conditionID int) {
+	n.children[conditionID] = childID
+}
+
+//define a Graph structure
+type Graph struct {
+	nodes map[int]Node
+}
+
 //function to create Action object
 func NewAction() Action {
 	return Action{parameters: map[string]string{}}
 }
 
-//function to create Condition object - necessary to not have nil pointer in initialised map
+//function to create Condition object
 func NewCondtion() Condition {
-	// return object of type condition and the map now initialised to be empty (not nil)
 	return Condition{pattern: map[string]string{}}
 }
 
 // function to create Node object
 func NewNode() Node {
-	return Node{conditions: map[int]Condition{}}
+	return Node{conditions: map[int]Condition{}, children: map[int]int{}}
+}
+
+//function to create graph object
+func NewGraph() Graph {
+	return Graph{nodes: map[int]Node{}}
 }
 
 func main() {
+	//open json file
+	jsonFile, _ := os.Open("Music--2018-08-10T09_20_31.449Z.json")
+	//catch if no file exists
+	if jsonFile == nil {
+		fmt.Println("no file in current directory")
+	}
 
-	fmt.Print("test")
+	//close the file so that it can be parsed later
+	defer jsonFile.Close()
 
-	cond := NewCondtion()
-	node := NewNode()
-	action := NewAction()
-
-	node.actions = append(node.actions, action)
-
-	cond.id = 3
-	node.id = 1
-
-	node.conditions[2] = cond
-
-	fmt.Print(node.actions[0])
+	fmt.Println(jsonFile)
 
 }

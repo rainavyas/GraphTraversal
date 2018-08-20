de1b91ba4ce7b5cc524961125dd8befe150b8cd5
TobyLi <toby@emotech.co>
2018-08-20 12:29:04 +0100

    unmarsheling json file done

--- parse_alg.go
+++ parse_alg.go
@@ -3,139 +3,174 @@ package main
 import (
 	"encoding/json"
 	"fmt"
-
 	"io/ioutil"
+	"os"
 )
 
 //define an Action structure
 type Action struct {
-	typ        string
-	command    string
-	parameters map[string]string
+	Typ        string
+	Command    string
+	Parameters map[string]string
 }
 
 //define a Condition structure
 type Condition struct {
-	id      int
-	typ     string
-	pattern map[string]string
+	Id      int
+	Typ     string
+	Pattern map[string]string
 }
 
 // define a node structure
 type Node struct {
-	id         int
-	typ        string
-	label      string
-	conditions map[int]Condition
-	actions    []Action
-	children   map[int]int
+	Id         int
+	Typ        string
+	Label      string
+	Conditions map[int]Condition
+	Actions    []Action
+	Children   map[int]int
 }
 
 //create method for Node class to be able to add children nodes
 func (n *Node) add_child(childID, conditionID int) {
-	n.children[conditionID] = childID
+	n.Children[conditionID] = childID
 }
 
 //define a Graph structure
 type Graph struct {
-	nodes map[int]Node
+	Nodes map[int]Node
 }
 
 //define a set of structures required for json unmarsheling
 
 //define json overall tree
-type json_nodes_and_trans struct {
-	nodes       []json_node       `json:"nodes"`
-	transitions []json_transition `json:"transitions"`
+type JsonNodesAndTrans struct {
+	Nodes       []JsonNode       `json:"nodes"`
+	Transitions []JsonTransition `json:"transitions"`
 }
 
 //define the json node structure
-type json_node struct {
-	id    int    `json:"id"`
-	typ   string `json:"type"`
-	label string `json:"label`
-	position json_position `json:"position"`
-	conditions []json_condition `json:"conditions"`
-	actions []json_action `json:"actions"`
-
+type JsonNode struct {
+	Id         int             `json:"id"`
+	Typ        string          `json:"type"`
+	Label      string          `json:"label`
+	Position   JsonPosition    `json:"position"`
+	Conditions []JsonCondition `json:"conditions"`
+	Actions    []JsonAction    `json:"actions"`
 }
 
 //define the json position structure
-type json_position{
-	x int `json:"x"`
-	y int `json:"y"`
+type JsonPosition struct {
+	X int `json:"x"`
+	Y int `json:"y"`
 }
 
 //define the json condition structure
-type json_condition struct{
-	id int `json:"id"`
-	typ int `json:"type"`
-	pattern []json_pattern `json:"pattern"`
+type JsonCondition struct {
+	Id      int           `json:"id"`
+	Typ     int           `json:"type"`
+	Pattern []JsonPattern `json:"pattern"`
 }
 
 //define the json pattern structure
-type json_pattern struct{
-	key string `json:"key"`
-	value string `json:"value"`
-
+type JsonPattern struct {
+	Key   string `json:"key"`
+	Value string `json:"value"`
 }
 
 //define the json action structure
-type json_action struct{
-	typ string `json:"type"`
-	command string `json:"command"`
-	parameters []json_parameter `json:"parameters"`
+type JsonAction struct {
+	Yyp        string          `json:"type"`
+	Command    string          `json:"command"`
+	Parameters []JsonParameter `json:"parameters"`
 }
 
-
 //define the json paramater structure
-type json_parameter struct{
-	key string `json:"key"`
-	value string `json"value"`
+type JsonParameter struct {
+	Key   string `json:"key"`
+	Value string `json"value"`
 }
 
 //define the json transition structure
+type JsonTransition struct {
+	From JsonFrom `json:"from"`
+	To   JsonTo   `json:"to"`
+}
 
+//define the json from structure
+type JsonFrom struct {
+	Node      int `json:"node"`
+	Condition int `json:"condition"`
+}
 
+//define the json to structure
+type JsonTo struct {
+	Node int `json:"node"`
+}
 
 //function to create Action object
 func NewAction() Action {
-	return Action{parameters: map[string]string{}}
+	return Action{Parameters: map[string]string{}}
 }
 
 //function to create Condition object
 func NewCondtion() Condition {
-	return Condition{pattern: map[string]string{}}
+	return Condition{Pattern: map[string]string{}}
 }
 
 // function to create Node object
 func NewNode() Node {
-	return Node{conditions: map[int]Condition{}, children: map[int]int{}}
+	return Node{Conditions: map[int]Condition{}, Children: map[int]int{}}
 }
 
-//function to create graph object
+//function to create Graph object
 func NewGraph() Graph {
-	return Graph{nodes: map[int]Node{}}
+	return Graph{Nodes: map[int]Node{}}
 }
 
 func main() {
 	//choose file
 	FileName := "jsons/Music--2018-08-10T09_20_31.449Z.json"
+	//FileName := "Music--2018-08-10T09_20_31.449Z.json"
 
 	//get json file in the form of json numbers
-	jsonFile, _ := ioutil.ReadFile(FileName)
+	jsonFile, err := os.Open(FileName)
 
-	//convert json numbers to a map, where key is string and value is a map with key string and value interface (not a fixed data structure)
-	var data map[string][]map[string]interface{}
-	json.Unmarshal(jsonFile, &data)
+	//if os.Open returns an error, handle it
+	if err != nil {
+		fmt.Println(err)
+	}
 
-	fmt.Println(data["nodes"][1]["position"])
+	//defer the closing of the jsonFile so that it can be parsed later
+	defer jsonFile.Close()
 
-	//check to see if the interface type can be set to a particular correct data structure
-	//var coord map[string]float64
-	//coord = data["nodes"][1]["position"]
+	//read the opened file as byte array
+	byteValue, err := ioutil.ReadAll(jsonFile)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	//initialise the json structures
+	JsonStructs := JsonNodesAndTrans{}
+
+	//umarshal the byteArray into the json structures defined
+	err = json.Unmarshal(byteValue, &JsonStructs)
+	if err != nil {
+		fmt.Println("error reached", err)
+	}
 
 	//store all the nodes in a Graph object
-	//graph := NewGraph()
+	CurrentNode:= NewNode() 
+	graph := NewGraph()
+	for i:=0; i < len(JsonStructs.Nodes){
+		//populate Node structure
+		CurrentNode.Id=JsonStructs.Nodes[i].Id
+		CurrentNode.Typ=JsonStructs.Nodes[i].Typ
+		CurrentNode.Label = JsonStructs.Nodes[i].Label
+		
+
+
+	}
+
 
 }
